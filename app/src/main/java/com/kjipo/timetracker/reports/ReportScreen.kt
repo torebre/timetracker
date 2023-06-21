e84b75ee1be2389d5fdb6478bdbae389d6f0a476
Tore Brede <tore.brede@gmail.com>
2023-06-21 20:09:46 +0200

    Working on tab for selecting time range for a report

--- app/src/main/java/com/kjipo/timetracker/reports/ReportScreen.kt
+++ app/src/main/java/com/kjipo/timetracker/reports/ReportScreen.kt
@@ -4,72 +4,77 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material3.DateRangePicker
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.rememberDateRangePickerState
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.Tab
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import java.io.File
-
-
-class ReportScreenInput(val uiState: ReportsUiState)
-
-
-class ReportScreenInputParameterProvider : PreviewParameterProvider<ReportScreenInput> {
-    override val values = sequenceOf(ReportScreenInput(ReportsUiState()))
-}
+import java.time.Duration
+import java.time.LocalDateTime
 
 
 @Composable
 fun ReportScreen(reportsModel: ReportsModel) {
     val uiState = reportsModel.uiState.collectAsState()
 
-    ReportScreen(ReportScreenInput(uiState.value))
-
+    ReportScreen(uiState.value) { selectedTimeRange ->
+        reportsModel.setSelectedTimeRange(selectedTimeRange)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ReportScreen(@PreviewParameter(PreviewParameterProvider::class) reportScreenInput: ReportScreenInput) {
+fun ReportScreen(uiState: ReportsUiState, onTabSelected: (SelectedTimeRange) -> Unit) {
     val context = LocalContext.current
-//    val state = rememberDateRangePickerState()
+    // TODO Update selected tab when user clicks on it
+    val selectedTab = remember { mutableStateOf(SelectedTimeRange.DAY) }
 
-    val state = rememberLazyListState()
     Column {
-
-        LazyColumn(state = state) {
-            for (projectSummary in reportScreenInput.uiState.projectSummaries) {
-                item {
-                    ProjectSummaryScreen(projectSummary)
-
+        ScrollableTabRow(selectedTabIndex = selectedTab.value.ordinal) {
+            SelectedTimeRange.values().forEachIndexed { index, timeRange ->
+                Tab(selected = index == selectedTab.value.ordinal,
+                    onClick = {
+                        onTabSelected(timeRange)
+                    }) {
+                    Text(text = timeRange.name)
                 }
-
             }
         }
 
-        Column(modifier = Modifier.fillMaxWidth()) {
+        val state = rememberLazyListState()
+        Column {
+            LazyColumn(state = state) {
+                for (projectSummary in uiState.projectSummaries) {
+                    item {
+                        ProjectSummaryScreen(projectSummary)
+                    }
+                }
+            }
+
+            Column(modifier = Modifier.fillMaxWidth()) {
 //        DateRangePicker(state = state, modifier = Modifier.weight(1f))
 
+                uiState.projectSummaries
 
-            reportScreenInput.uiState.projectSummaries
+                // TODO Get file to export to
 
-
-            // TODO Get file to export to
-
-            // TODO Just here for testing
+                // TODO Just here for testing
 //    CalendarComponent(calendarUiState = CalendarUiState())
 
-            val fileToExportTo = File(context.cacheDir, "export_file_temp.zip")
-            Button(onClick = { exportData(fileToExportTo, context) }) {
-                Text("Export")
+                val fileToExportTo = File(context.cacheDir, "export_file_temp.zip")
+                Button(onClick = { exportData(fileToExportTo, context) }) {
+                    Text("Export")
+                }
             }
+
         }
 
     }
@@ -80,4 +85,37 @@ fun ProjectSummaryScreen(projectSummary: ProjectSummary) {
     Text(projectSummary.title)
     Text("${projectSummary.duration}")
     Text("${projectSummary.percentage}")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ReportScreenPreview() {
+    val pieChartData = PieChartData(
+        listOf(
+            PieChartEntry(1, 10, Color.Green),
+            PieChartEntry(2, 50, Color.Yellow)
+        )
+    )
+
+    val projectSummaries = listOf(
+        ProjectSummary(
+            1L,
+            "Test project",
+            Duration.ofMinutes(100),
+            10.0
+        )
+    )
+
+    val uiState = ReportsUiState(
+        selectedTimeRange = SelectedTimeRange.DAY,
+        pieChartData = pieChartData,
+        projectSummaries = projectSummaries,
+        customRange = DateRange(LocalDateTime.now().minusDays(1), LocalDateTime.now())
+    )
+
+    ReportScreen(uiState, {
+        // Do nothing
+    })
+
 }
