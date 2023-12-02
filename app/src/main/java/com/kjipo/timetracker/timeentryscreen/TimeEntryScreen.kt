1065f200d09d25c04090f0804caadae35fa356de
Tore Brede <tore.brede@gmail.com>
2023-12-02 17:40:08 +0100

    Using material3 imports

--- app/src/main/java/com/kjipo/timetracker/timeentryscreen/TimeEntryScreen.kt
+++ app/src/main/java/com/kjipo/timetracker/timeentryscreen/TimeEntryScreen.kt
@@ -6,11 +6,11 @@ import android.widget.TimePicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
