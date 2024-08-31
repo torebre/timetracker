0fa38265a9a77f00a09738a9cbff5519fe795601
Tore Brede <tore.brede@gmail.com>
2024-08-31 15:29:18 +0200

    Fixing some imports to be from material3

--- app/src/main/java/com/kjipo/timetracker/tagscreen/TagScreen.kt
+++ app/src/main/java/com/kjipo/timetracker/tagscreen/TagScreen.kt
@@ -2,9 +2,9 @@ package com.kjipo.timetracker.tagscreen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
